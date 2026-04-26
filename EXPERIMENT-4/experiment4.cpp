54f399fccd88ec3e666e0f8cb2d48405949031f0
Harshit Kumawat <goldie310805@gmail.com>
2026-04-26 12:06:09 +0530

    Create experiment4.cpp

---
+++ EXPERIMENT-4/experiment4.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+
+        if (n == 2) {
+            cout << -1 << "\n";
+            continue;
+        }
+
+        vector<int> vals;
+        for (int i = 1; i <= n*n; i += 2)
+            vals.push_back(i);
+        for (int i = 2; i<=n*n; i += 2)
+            vals.push_back(i);
+        int idx = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                cout << vals[idx++] << " ";
+            }
+            cout << "\n";
+        }
+    }
+
+    return 0;
+}
